78e3d672896cf5226972b95fd0d0d0e03025682c
dmitry-vlasov <vlasov@academ.org>
2019-11-13 15:55:58 +0700

    some mm stuff is added

--- compile_grammars.sh
+++ compile_grammars.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-pegcompiler flowparser=parse/ru_parse_file.flow flowparserast=parse/ru_parse_ast.flow file=parse/russell.lingo
+pegcompile flowparser=parse/ru_parse_file.flow flowparserast=parse/ru_parse_ast.flow file=parse/russell.lingo
+pegcompile flowparser=parse/mm_parse_file.flow flowparserast=parse/mm_parse_ast.flow file=parse/metamath.lingo
