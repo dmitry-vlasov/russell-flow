c2f2f861bdd59eadcf364a5a0cb558357713b027
dmitry-vlasov <vlasov@academ.org>
2019-11-23 17:39:54 +0700

    imports optimization is added

--- math/transl_test.sh
+++ math/transl_test.sh
@@ -4,7 +4,7 @@ file=$1
 
 rm $file-1.mm
 
-../bin/russell verbose=1 $file.mm mm-2-ru=1 ru-root=. ru-write-all=1 mm-stats=1 mm-fix=1 mm-split=1
+../bin/russell verbose=1 $file.mm mm-2-ru=1 ru-root=. ru-write-all=1 mm-stats=1 mm-fix=1 mm-split=1 ru-imports=1
 
 ../bin/russell verbose=1 $file.ru mm=$file-1.mm ru-2-mm=1 stats=1
 
