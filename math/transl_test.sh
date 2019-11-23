e21a948441ce8a0c9254069c1b7be56bfabc6429
dmitry-vlasov <vlasov@academ.org>
2019-11-23 18:21:40 +0700

    concurrency enhanced

--- math/transl_test.sh
+++ math/transl_test.sh
@@ -6,6 +6,6 @@ rm $file-1.mm
 
 ../bin/russell verbose=1 $file.mm mm-2-ru=1 ru-root=. ru-write-all=1 mm-stats=1 mm-fix=1 mm-split=1 ru-imports=1
 
-../bin/russell verbose=1 $file.ru mm=$file-1.mm ru-2-mm=1 stats=1
+../bin/russell verbose=2 $file.ru mm=$file-1.mm ru-2-mm=1 ru-stats=1
 
 ../bin/metamath "read $file-1.mm" 'verify proof *' 'exit'
