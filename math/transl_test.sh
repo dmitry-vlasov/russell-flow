cf4da5c09cc39f2fae390fecd7ce304fa1d3033b
dmitry-vlasov <vlasov@academ.org>
2020-02-18 13:43:42 +0700

    caching may be switched off

--- math/transl_test.sh
+++ math/transl_test.sh
@@ -4,8 +4,8 @@ file=$1
 
 rm $file-1.mm
 
-../bin/russell verbose=1 $file.mm mm-2-ru=1 ru-root=. ru-write-all=1 mm-stats=1 mm-fix=1 mm-split=1 ru-imports=1
+../bin/russell verbose=1 caching=0 $file.mm mm-2-ru=1 ru-root=. ru-write-all=1 mm-stats=1 mm-fix=1 mm-split=1 ru-imports=1
 
-../bin/russell verbose=1 $file.ru mm=$file-1.mm ru-2-mm=1 ru-stats=1
+../bin/russell verbose=1 caching=0 $file.ru mm=$file-1.mm ru-2-mm=1 ru-stats=1
 
 ../bin/metamath "read $file-1.mm" 'verify proof *' 'exit'
