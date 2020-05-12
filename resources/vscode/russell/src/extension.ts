2772061dec22b48b129afac7d2211471e00d5a01
Dmitry Vlasov <vlasov@academ.org>
2020-05-12 13:32:15 +0700

    vscode extension is added

---
+++ resources/vscode/russell/src/extension.ts
@@ -0,0 +1,204 @@
+'use strict';
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import * as path from 'path';
+import { ChildProcess } from 'child_process';
+import * as fs from "fs";
+import * as PropertiesReader from 'properties-reader';
+import {
+    LanguageClient, LanguageClientOptions, ServerOptions, RevealOutputChannelOn,
+} from 'vscode-languageclient';
+import * as tools from "./tools";
+
+const isPortReachable = require('is-port-reachable');
+
+let client: LanguageClient = null;
+let russellChannel : vscode.OutputChannel = null;
+let serverStatusBarItem: vscode.StatusBarItem;
+let httpServer : ChildProcess;
+let httpServerOnline : boolean = false;
+
+export function activate(context: vscode.ExtensionContext) {	
+	serverStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+	serverStatusBarItem.command = 'russell.toggleHttpServer';
+    context.subscriptions.push(serverStatusBarItem);
+    context.subscriptions.push(vscode.commands.registerCommand('russell.verify', verifyRussell));
+    context.subscriptions.push(vscode.commands.registerCommand('russell.metamath', verifyMetamath));
+    context.subscriptions.push(vscode.commands.registerCommand('russell.startHttpServer', startHttpServer));
+	context.subscriptions.push(vscode.commands.registerCommand('russell.stopHttpServer', stopHttpServer));
+	context.subscriptions.push(vscode.commands.registerCommand('russell.restartLspServer', restartLspServer));
+	context.subscriptions.push(vscode.commands.registerCommand('russell.toggleHttpServer', toggleHttpServer));
+
+    russellChannel = vscode.window.createOutputChannel("Russell");
+	russellChannel.show();
+
+	checkHttpServerStatus(true);
+	setInterval(checkHttpServerStatus, 3000, false);
+	serverStatusBarItem.show();
+
+    // Create a client
+	startLspServer();
+}
+
+function startLspServer() {
+	// If the extension is launched in debug mode then the debug server options are used
+	// Otherwise the run options are used
+	const root_dir : string = vscode.workspace.getConfiguration("russell").get("lspRoot");
+	let serverOptions: ServerOptions = {
+		command: process.platform == "win32" ? 'russell.bat' : 'russell',
+		args: ['server=lsp'],
+		options:  (root_dir == "") ? { detached: false } : { detached: false, cwd:  root_dir }
+	}
+	// Options to control the language client
+	let clientOptions: LanguageClientOptions = {
+		// Register the server for plain text documents
+		documentSelector: [{scheme: 'file', language: 'russell'}],
+		outputChannel: russellChannel,
+		revealOutputChannelOn: RevealOutputChannelOn.Info,
+		uriConverters: {
+			// FIXME: by default the URI sent over the protocol will be percent encoded (see rfc3986#section-2.1)
+			//        the "workaround" below disables temporarily the encoding until decoding
+			//        is implemented properly in clangd
+			code2Protocol: (uri: vscode.Uri) : string => uri.toString(true),
+			protocol2Code: (uri: string) : vscode.Uri => vscode.Uri.parse(uri)
+		}
+	}
+
+	// Create the language client and start the client.
+	client = new LanguageClient('russell', 'Russell Language Server', serverOptions, clientOptions);
+	// Start the client. This will also launch the server
+	client.start();
+	russellChannel.appendLine('Russell LSP server stared');
+	client.onReady().then(() => russellChannel.appendLine('Russell LSP server is ready'));
+}
+
+function restartLspServer() {
+	if (client) {
+		client.sendNotification("exit");
+        client.stop().then(startLspServer);
+	} else {
+		startLspServer();
+	}
+}
+
+function checkHttpServerStatus(initial : boolean) {
+	const port = vscode.workspace.getConfiguration("russell").get("portOfHttpServer");
+	isPortReachable(port, {host: 'localhost'}).then(
+		(reacheable : boolean) => {
+			if (reacheable) {
+				showHttpServerOnline();
+				httpServerOnline = true;
+			} else {
+				httpServer = null;
+				httpServerOnline = false;
+				showHttpServerOffline();
+				if (initial) {
+					// launch russell server at startup
+					let autostart = vscode.workspace.getConfiguration("russell").get("autostartHttpServer");
+					if (autostart) {
+						startHttpServer();
+					}
+				}
+			}
+		}
+	);
+}
+
+function toggleHttpServer() {
+    if (!httpServerOnline) {
+		startHttpServer();
+    } else {
+		stopHttpServer();
+	}
+}
+
+function startHttpServer() {
+    if (!httpServerOnline) {
+		const port : number = vscode.workspace.getConfiguration("russell").get("portOfHttpServer");
+		const root_dir : string = vscode.workspace.getConfiguration("russell").get("serverRoot");
+		httpServer = tools.launchHttpServer(root_dir, port, showHttpServerOnline, showHttpServerOffline);
+		httpServerOnline = true;
+    }
+}
+
+function stopHttpServer() {
+	if (httpServerOnline) {
+		const port : number = vscode.workspace.getConfiguration("russell").get("portOfHttpServer");
+		tools.shutdownHttpServer(port).on("exit", (code, msg) => httpServer = null);
+		httpServerOnline = false;
+	}
+}
+
+function showHttpServerOnline() {
+	serverStatusBarItem.text = `$(vm-active) russell http server: online`;
+}
+
+function showHttpServerOffline() {
+	serverStatusBarItem.text = `$(vm-outline) russell: http server: offline`;
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+	// First, shutdown Russellc server, if it is owned by current vscode instance
+	if (httpServer) {
+		const port : number = vscode.workspace.getConfiguration("russell").get("portOfHttpServer");
+		tools.shutdownHttpServer(port).on("exit", (code, msg) => httpServer = null);
+	}
+    if (!client) {
+        return undefined;
+    } else {
+        client.sendNotification("exit");
+        return client.stop()
+    }
+}
+
+const homedir = process.env[(process.platform == "win32") ? "USERPROFILE" : "HOME"];
+
+function expandHomeDir(p : string) : string {
+	if (!p) return p;
+	if (p == "~") return homedir;
+	if (p.slice(0, 2) != "~/") return p;
+	return path.join(homedir, p.slice(2));
+}
+
+function getPath(uri : string | vscode.Uri) : string {
+	return expandHomeDir(uri instanceof vscode.Uri ? uri.fsPath : uri.startsWith("file://") ? vscode.Uri.parse(uri).fsPath : uri);
+}
+
+function resolveProjectRoot(uri : string | vscode.Uri) : string {
+	const config = vscode.workspace.getConfiguration("russell");
+	if (uri != null) {
+		let dir = uri != null ? getPath(uri) : path.resolve(getPath(config.get("projectRoot")), "russell.conf");
+		while (dir != path.resolve(dir, "..")) {
+			dir = path.resolve(dir, "..");
+			if (fs.existsSync(path.resolve(dir, "russell.conf"))) {
+				return dir;
+			}
+		}
+	}
+	return getPath(config.get("root"));
+}
+
+function verifyRussell() {
+	let document = vscode.window.activeTextEditor.document;
+	let rootPath = resolveProjectRoot(document.uri);
+	russellChannel.clear();
+	russellChannel.show(true);
+	russellChannel.appendLine("Verifying directory '" + document.uri + "'");
+	tools.run_cmd("russell", rootPath, ["verify=1"], (s : string) => russellChannel.append(s.toString()));
+}
+
+function verifyMetamath() {
+    verifyRussell();
+}
+
+// reads configuration, defaults to global plugin configuration
+function readConfiguration(): PropertiesReader.Reader {
+    let configFile = path.join(vscode.workspace.rootPath, "russell.config");
+	var reader = PropertiesReader(undefined);
+	if (fs.existsSync(configFile)) {
+		reader.append(configFile);
+	}
+    return reader;
+}
---
+++ resources/vscode/russell/src/tools.ts
@@ -0,0 +1,31 @@
+import { spawn, ChildProcess, spawnSync } from 'child_process';
+
+export function run_cmd(cmd: string, wd: string, args: string[], on_out: (string) => void): ChildProcess {
+    const options = wd && wd.length > 0 ? { cwd: wd, shell: true, encoding: 'utf8'} : { shell : true, encoding: 'utf8'};
+    let child = spawn(cmd, args, options);
+    child.stdout.setEncoding('utf8');
+    child.stdout.on("data", on_out);
+    child.stderr.on("data", on_out);
+    //child.on("close", (code, msg) => on_exit(code, msg));
+    return child;
+}
+
+export function run_cmd_sync(cmd: string, wd: string, args: string[]) {
+	const options = wd && wd.length > 0 ? 
+		{ cwd: wd, shell: true, encoding: 'utf8' } :
+        { shell: true, encoding: 'utf8' };
+    return spawnSync(cmd, args, options);
+}
+
+export function shutdownHttpServer(port : number) {
+    return run_cmd("russell", "", ["shutdown-server=1", "server-port=" + port], (s) => { console.log(s); } );
+}
+
+export function launchHttpServer(projectRoot: string, port : number, on_start : () => void, on_stop : () => void) {
+    on_start();
+    let httpServer = run_cmd("russell", projectRoot, ["server=http", "server-port=" + port], (s) => { console.log(s); });
+    httpServer.addListener("close", (code: number, signal: string) => { on_stop() });
+    httpServer.addListener("disconnect", () => { on_stop() });
+    httpServer.addListener("exit", (code: number, signal: string) => { on_stop() });
+    return httpServer
+}
