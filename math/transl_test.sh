abbc6aa5764910523079c34fba8d7f6327215ac6
Dmitry Vlasov <vlasov@academ.org>
2020-05-31 22:46:03 +0700

    translation restored

--- math/transl_test.sh
+++ math/transl_test.sh
@@ -4,8 +4,33 @@ file=$1
 
 rm $file-1.mm
 
-../bin/russell verbose=1 caching=1 $file.mm mm-2-ru=1 ru-root=. ru-write-all=1 mm-stats=1 mm-fix=1 mm-split=1 ru-imports=1
+../bin/russell \
+	update-conf verbose=1 ru-root=./${file} ';' \
+	mem-stats ';' \
+	read file=${file}.mm ';' \
+	mm-to-ru file=${file}.mm ';' \
+	mem-stats ';' \
+	fix-left-recursion file=${file}.ru ';' \
+	split-math file=${file}.ru ';' \
+	mem-stats ';' \
+	math-stats ';' \
+	write-ru all=1 ';' \
+	mem-stats
 
-../bin/russell verbose=1 caching=1 $file.ru mm=$file-1.mm ru-2-mm=1 ru-stats=1
+dir=$(pwd)
+
+echo import-roots=${dir}/${file} > ./${file}/russell.conf
+
+#verbose=1 caching=1 $file.ru mm=$file-1.mm ru-2-mm=1 ru-stats=1
+../bin/russell \
+	load-conf dir=./${file} \
+	update-conf verbose=1 ru-root=./${file} ';' \
+	mem-stats ';' \
+	read file=./${file}/${file}.ru ';' \
+	math-stats ';' \
+	ru-to-mm file=${file}.ru ';' \
+	mem-stats ';' \
+	write-mm file=${file}-1.mm \
+	mem-stats ';'
 
 ../bin/metamath "read $file-1.mm" 'verify proof *' 'exit'
