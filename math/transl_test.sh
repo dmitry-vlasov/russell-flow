050dc39b92fc99d8e446fe3ad91e2f49a68fb0e7
Dmitry Vlasov <vlasov@academ.org>
2020-06-01 15:56:05 +0700

    set.mm passes

--- math/transl_test.sh
+++ math/transl_test.sh
@@ -10,26 +10,31 @@ if [ -d "./${file}" ]; then
 	rm -fr ./${file}
 fi
 
+mkdir ./${file}
+
 echo "Translation: Metamath --> Russell"
 echo "---------------------------------"
 echo
 
+dir=$(pwd)
+
+echo import-roots=${dir}/${file} > ./${file}/russell.conf
+
 ../bin/russell \
-	update-conf verbose=1 ru-root=./${file} ";" \
+	update-conf verbose=1 ru-root=./${file} import-roots=${dir} ";" \
 	mem-stats ";" \
 	read file=${file}.mm ";" \
 	mm-to-ru file=${file}.mm ";" \
 	mem-stats ";" \
+	clear-conf ";" \
+	update-conf verbose=1 ru-root=./${file} import-roots=${dir}/${file} ";" \
 	fix-left-recursion file=${file}.ru ";" \
 	split-math file=${file}.ru ";" \
 	optimize-imports ";" \
 	verify target=all ";" \
 	mem-stats ";" \
+	write-ru all=1 ";" \
 	math-stats
-
-dir=$(pwd)
-
-echo import-roots=${dir}/${file} > ./${file}/russell.conf
 
 echo
 echo "Translation: Russell --> Metamath"
