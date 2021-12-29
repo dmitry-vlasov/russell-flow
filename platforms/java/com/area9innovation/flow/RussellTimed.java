ad3d4d604bf50b305378acab68c0dfa628a8f899
Dmitry Vlasov <vlasov@academ.org>
2021-12-29 17:43:36 +0700

    RuTaskTimer is introduced

--- platforms/java/com/area9innovation/flow/RussellTimed.java
+++ platforms/java/com/area9innovation/flow/RussellTimed.java
@@ -9,20 +9,22 @@ import java.util.Set;
 
 public class RussellTimed extends NativeHost {
 	@SuppressWarnings (value="unchecked")
-	public static final <K, V> Object iterHashMapTimed(Object map, Func2<Object, K, V> fn, double time_limit) {
+	public static final <K, V> boolean iterHashMapTimed(Object map, Func2<Object, K, V> fn, double time_limit) {
 		HashMap<K, V> hmap = (HashMap<K, V>)map;
 		final long started = System.currentTimeMillis();
 		final long limit = (time_limit > 0) ? started + (long)time_limit : -1;
 		final Set<Map.Entry<K, V>> set = hmap.entrySet();
 		final Iterator<Map.Entry<K, V>> iter = set.iterator();
+		boolean interrupted = false;
 		while (iter.hasNext()) {
 			Map.Entry<K, V> entry = iter.next();
 			fn.invoke(entry.getKey(), entry.getValue());
 			if (limit > 0 && System.currentTimeMillis() > limit) {
+				interrupted = true;
 				break;
 			}
 		}
-		return null;
+		return interrupted;
 	}
 
 	public static final Object[] rvector2array(Object v) {
