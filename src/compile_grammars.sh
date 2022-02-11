6cccd5a38b9d0ad01c720d1cff7d735028e0a726
Dmitry Vlasov <vlasov@academ.org>
2022-02-11 13:06:26 +0700

    runtime prepared, yet doesn't work

--- src/compile_grammars.sh
+++ src/compile_grammars.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-flowcpp --batch lingo/pegcode/pegcompiler.flow -- flowparser=ru/parse/file.flow flowparserast=ru/parse/ast.flow file=ru/parse/russell.lingo
-flowcpp --batch lingo/pegcode/pegcompiler.flow -- flowparser=mm/parse/file.flow flowparserast=mm/parse/ast.flow file=mm/parse/metamath.lingo
+flowcpp --batch lingo/pegcode/pegcompiler.flow -- flowparser=ru/parse/file.flow flowparserast=ru/parse/ast.flow file=ru/parse/russell.lingo prefix_rules=ru
+flowcpp --batch lingo/pegcode/pegcompiler.flow -- flowparser=mm/parse/file.flow flowparserast=mm/parse/ast.flow file=mm/parse/metamath.lingo prefix_rules=mm
