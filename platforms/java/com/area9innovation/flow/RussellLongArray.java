d5be551935b9a4d37300eb2ecbc32e1ecd277466
Dmitry Vlasov <vlasov@academ.org>
2023-11-19 20:02:22 +0100

    a bunch of natives is added to long array

--- platforms/java/com/area9innovation/flow/RussellLongArray.java
+++ platforms/java/com/area9innovation/flow/RussellLongArray.java
@@ -30,4 +30,70 @@ public class RussellLongArray extends NativeHost {
 		rv[a.length] = (long)high << 32 + (long)low;
 		return rv;
 	}
+	@SuppressWarnings("unchecked")
+	public static final <T> Object[] map(long[] arr, Func2<T, Integer, Integer> clos) {
+		Object[] rv = new Object[arr.length];
+		for (int i = 0; i < arr.length; i++) {
+			rv[i] = clos.invoke(arr[i] & 0xFFFFFFFF, arr[i] >> 32);
+		}
+		return rv;
+	}
+
+	@SuppressWarnings("unchecked")
+	public static final Object iter(long[] arr, Func2<Object, Integer, Integer> clos) {
+		for (int i = 0; i < arr.length; i++)
+			clos.invoke(arr[i] & 0xFFFFFFFF, arr[i] >> 32);
+		return null;
+	}
+
+	@SuppressWarnings("unchecked")
+	public static final <T> Object[] mapi(long[] arr, Func3<T, Integer, Integer, Integer> clos) {
+		Object[] rv = new Object[arr.length];
+		for (int i = 0; i < arr.length; i++)
+			rv[i] = clos.invoke(i, arr[i] & 0xFFFFFFFF, arr[i] >> 32);
+		return rv;
+	}
+
+	@SuppressWarnings("unchecked")
+	public static final Object iteri(long[] arr, Func3<Object, Integer, Integer, Integer> clos) {
+		for (int i = 0; i < arr.length; i++)
+			clos.invoke(i, arr[i] & 0xFFFFFFFF, arr[i] >> 32);
+		return null;
+	}
+
+	@SuppressWarnings("unchecked")
+	public static final int iteriUntil(long[] arr, Func3<Boolean, Integer, Integer, Integer> clos) {
+		for (int i = 0; i < arr.length; i++)
+			if (clos.invoke(i, arr[i] & 0xFFFFFFFF, arr[i] >> 32))
+				return i;
+		return arr.length;
+	}
+
+	@SuppressWarnings("unchecked")
+	public static final <T> T fold(long[] arr, T init, Func3<T, T, Integer, Integer> clos) {
+		for (int i = 0; i < arr.length; i++)
+			init = clos.invoke(init, arr[i] & 0xFFFFFFFF, arr[i] >> 32);
+		return init;
+	}
+
+	@SuppressWarnings("unchecked")
+	public static final <T> T foldi(long[] arr, T init, Func4<T, Integer, T, Integer, Integer> clos) {
+		for (int i = 0; i < arr.length; i++)
+			init = clos.invoke(i, init, arr[i] & 0xFFFFFFFF, arr[i] >> 32);
+		return init;
+	}
+
+	@SuppressWarnings("unchecked")
+	public static final  long[] filter(long[] arr, Func2<Boolean, Integer, Integer> test) {
+		boolean[] tmp = new boolean[arr.length];
+		int count = 0;
+		for (int i = 0; i < arr.length; i++)
+			if (tmp[i] = test.invoke(arr[i] & 0xFFFFFFFF, arr[i] >> 32))
+				count++;
+		long[] out = new long[count];
+		for (int i = 0, j = 0; i < arr.length; i++)
+			if (tmp[i])
+				out[j++] = arr[i];
+		return out;
+	}
 }
