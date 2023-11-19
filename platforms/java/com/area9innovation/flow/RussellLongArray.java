6f1ed6f1c5b8d82a323c5144fcb355b913ca01a2
Dmitry Vlasov <vlasov@academ.org>
2023-11-19 20:31:16 +0100

    LongArray access is made via Long native

--- platforms/java/com/area9innovation/flow/RussellLongArray.java
+++ platforms/java/com/area9innovation/flow/RussellLongArray.java
@@ -8,87 +8,97 @@ public class RussellLongArray extends NativeHost {
 		public int beg;
 		public int end;
 	}
+	static public final long makeLong(int low, int high) {
+		return (long)high << 32 + (long)low;
+	}
+	static public final int longLow(long l) {
+		return (int)l & 0xFFFFFFFF;
+	}
+	static public final int longHigh(long l) {
+		return (int)l >> 32;
+	}
+
 	static public final long[] make(int size) { return new long[size]; }
 	static public final int size(long[] arr) { return arr.length; }
-	static public final int getLow(long[] arr, int i) { return (int)arr[i] & 0xFFFFFFFF; }
-	static public final int getHigh(long[] arr, int i) { return (int)arr[i] >> 32; }
-	static public final void set(long[] arr, int i, int low, int high) { arr[i] = (long)high << 32 + (long)low; }
+	static public final long get(long[] arr, int i) { return arr[i]; }
+	static public final Object set(long[] arr, int i, long v) { arr[i] = v; }
+
 	public static final long[] concat(long[] a, long[] b) {
 		long[] rv = Arrays.copyOf(a, a.length + b.length);
 		System.arraycopy(b, 0, rv, a.length, b.length);
 		return rv;
 	}
-	public static final long[] pushFront(int low, int high, long[] b) {
+	public static final long[] pushFront(Object v, long[] b) {
 		long[] rv = new long[b.length + 1];
-		rv[0] = (long)high << 32 + (long)low;
+		rv[0] = (long)v;
 		System.arraycopy(b, 0, rv, 1, b.length);
 		return rv;
 	}
-	public static final long[] pushBack(long a[], int low, int high) {
+	public static final long[] pushBack(long a[], Object v) {
 		long[] rv = new long[a.length + 1];
 		System.arraycopy(a, 0, rv, 0, a.length);
-		rv[a.length] = (long)high << 32 + (long)low;
+		rv[a.length] = (long)v;
 		return rv;
 	}
 	@SuppressWarnings("unchecked")
-	public static final <T> Object[] map(long[] arr, Func2<T, Integer, Integer> clos) {
+	public static final <T> Object[] map(long[] arr, Func1<T, Object> clos) {
 		Object[] rv = new Object[arr.length];
 		for (int i = 0; i < arr.length; i++) {
-			rv[i] = clos.invoke(arr[i] & 0xFFFFFFFF, arr[i] >> 32);
+			rv[i] = clos.invoke(arr[i]);
 		}
 		return rv;
 	}
 
 	@SuppressWarnings("unchecked")
-	public static final Object iter(long[] arr, Func2<Object, Integer, Integer> clos) {
+	public static final Object iter(long[] arr, Func1<Object, Object> clos) {
 		for (int i = 0; i < arr.length; i++)
-			clos.invoke(arr[i] & 0xFFFFFFFF, arr[i] >> 32);
+			clos.invoke(arr[i]);
 		return null;
 	}
 
 	@SuppressWarnings("unchecked")
-	public static final <T> Object[] mapi(long[] arr, Func3<T, Integer, Integer, Integer> clos) {
+	public static final <T> Object[] mapi(long[] arr, Func2<T, Integer, Object> clos) {
 		Object[] rv = new Object[arr.length];
 		for (int i = 0; i < arr.length; i++)
-			rv[i] = clos.invoke(i, arr[i] & 0xFFFFFFFF, arr[i] >> 32);
+			rv[i] = clos.invoke(arr[i]);
 		return rv;
 	}
 
 	@SuppressWarnings("unchecked")
-	public static final Object iteri(long[] arr, Func3<Object, Integer, Integer, Integer> clos) {
+	public static final Object iteri(long[] arr, Func2<Object, Integer, Object> clos) {
 		for (int i = 0; i < arr.length; i++)
-			clos.invoke(i, arr[i] & 0xFFFFFFFF, arr[i] >> 32);
+			clos.invoke(i, arr[i]);
 		return null;
 	}
 
 	@SuppressWarnings("unchecked")
-	public static final int iteriUntil(long[] arr, Func3<Boolean, Integer, Integer, Integer> clos) {
+	public static final int iteriUntil(long[] arr, Func2<Boolean, Integer, Object> clos) {
 		for (int i = 0; i < arr.length; i++)
-			if (clos.invoke(i, arr[i] & 0xFFFFFFFF, arr[i] >> 32))
+			if (clos.invoke(i, arr[i]))
 				return i;
 		return arr.length;
 	}
 
 	@SuppressWarnings("unchecked")
-	public static final <T> T fold(long[] arr, T init, Func3<T, T, Integer, Integer> clos) {
+	public static final <T> T fold(long[] arr, T init, Func2<T, T, Object> clos) {
 		for (int i = 0; i < arr.length; i++)
-			init = clos.invoke(init, arr[i] & 0xFFFFFFFF, arr[i] >> 32);
+			init = clos.invoke(init, arr[i]);
 		return init;
 	}
 
 	@SuppressWarnings("unchecked")
-	public static final <T> T foldi(long[] arr, T init, Func4<T, Integer, T, Integer, Integer> clos) {
+	public static final <T> T foldi(long[] arr, T init, Func3<T, Integer, T, Object> clos) {
 		for (int i = 0; i < arr.length; i++)
-			init = clos.invoke(i, init, arr[i] & 0xFFFFFFFF, arr[i] >> 32);
+			init = clos.invoke(i, init, arr[i]);
 		return init;
 	}
 
 	@SuppressWarnings("unchecked")
-	public static final  long[] filter(long[] arr, Func2<Boolean, Integer, Integer> test) {
+	public static final  long[] filter(long[] arr, Func1<Boolean, Object> test) {
 		boolean[] tmp = new boolean[arr.length];
 		int count = 0;
 		for (int i = 0; i < arr.length; i++)
-			if (tmp[i] = test.invoke(arr[i] & 0xFFFFFFFF, arr[i] >> 32))
+			if (tmp[i] = test.invoke(arr[i]))
 				count++;
 		long[] out = new long[count];
 		for (int i = 0, j = 0; i < arr.length; i++)
